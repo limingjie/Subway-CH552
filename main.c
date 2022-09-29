b15cac547bf60daa3b7eb94f283d4b9f88341de3
Mingjie Li <limingjie@outlook.com>
2022-09-29 22:39:43 +0800

    Rewrite runSubway() & blinkLights()

--- main.c
+++ main.c
@@ -15,23 +15,22 @@
 // All key pins are pulled up.
 // - A key pressed if input bit changes from 1 to 0.
 // - A key released if input bit changes from 0 to 1.
-#define KEY_PRESSED(KEY)  ((previousP1 & (1 << (KEY))) && !(P1 & (1 << (KEY))))
-#define KEY_RELEASED(KEY) (!(previousP1 & (1 << (KEY))) && (P1 & (1 << (KEY))))
-#define DEBOUNCE_INTERVAL 8
+#define KEY_PRESSED(KEY)  ((lastP1 & (1 << (KEY))) && !(P1 & (1 << (KEY))))
+#define KEY_RELEASED(KEY) (!(lastP1 & (1 << (KEY))) && (P1 & (1 << (KEY))))
+#define DEBOUNCE_INTERVAL 8  // 8 ms
 
 // Output Pins
-#define LED_PIN   0  // P3.0 - WS2812 LED Strip
-#define LATCH_PIN 2  // P3.2 - Power latch
+#define LED_PIN  0  // P3.0 - WS2812 LED Strip
+#define KILL_PIN 2  // P3.2 - Power control
 
 // Define bits
-SBIT(LED, 0xB0, LED_PIN);      // P3.0
-SBIT(LATCH, 0xB0, LATCH_PIN);  // P3.2
+SBIT(LED, 0xB0, LED_PIN);    // P3.0
+SBIT(KILL, 0xB0, KILL_PIN);  // P3.2
 
 // WS2812
 #define LED_COUNT  27
 #define BRIGHTNESS 15
-// External data memory
-__xdata uint8_t      ledData[LED_COUNT * 3];
+__xdata uint8_t      ledData[LED_COUNT * 3];  // In external data memory
 __bit                ledChanged = 0;
 __data const uint8_t RED[3]     = {BRIGHTNESS, 0, 0};
 __data const uint8_t GREEN[3]   = {0, BRIGHTNESS, 0};
@@ -39,34 +38,45 @@ __data const uint8_t BLUE[3]    = {0, 0, BRIGHTNESS};
 __data const uint8_t PURPLE[3]  = {BRIGHTNESS, 0, BRIGHTNESS};
 __data const uint8_t BLACK[3]   = {0, 0, 0};
 
-#define SUBWAY_COLOR         0
-#define SUBWAY_ARRIVAL_COLOR 1
-
 typedef struct
 {
-    const uint8_t         stops[25];
-    const uint8_t         length;
-    uint8_t               at;
-    uint8_t               arrives;
-    uint8_t               blinkTimer;
-    __data const uint8_t *colors[2];  // `__data` -> 8-bit pointer
+    const uint8_t               stops[25];
+    const uint8_t               length;
+    uint8_t                     at;
+    uint8_t                     running;       // __bit will be promoted to int here
+    __data const uint8_t *const color;         // `__data` -> 8-bit pointer
+    __data const uint8_t *const arrivalColor;  // `__data` -> 8-bit pointer
 } Subway;
 
 __data Subway line[2] = {
-    {{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, 10, 0, 0, 0, {GREEN, RED}},
-    {{5, 6, 7, 8, 9}, 5, 0, 0, 0, {BLUE, PURPLE}},
+    {
+        {0, 1, 2, 3, 4, 5, 6, 7, 8, 9},  // .stops
+        10,                              // .length
+        0,                               // .at
+        1,                               // .running
+        GREEN,                           // .color
+        RED,                             // .arrivalColor
+    },
+    {
+        {5, 6, 7, 8, 9},  // .stops
+        5,                // .length
+        0,                // .at
+        1,                // .running
+        BLUE,             // .color
+        PURPLE,           // .arrivalColor
+    },
 };
 
-uint8_t gates[2] = {10, 11};
+__data const uint8_t gates[2] = {10, 11};
 
 uint16_t idleTimer = 0;
 
 void setColor(uint8_t index, __data const uint8_t *color)  // `__data` -> 8-bit pointer
 {
     index *= 3;
-    ledData[index++] = color[1];
-    ledData[index++] = color[0];
-    ledData[index]   = color[2];
+    ledData[index]     = color[1];
+    ledData[index + 1] = color[0];
+    ledData[index + 2] = color[2];
 }
 
 void initSubway()
@@ -79,13 +89,24 @@ void initSubway()
 
 void runSubway(uint8_t i, __bit forward)
 {
-    __data Subway *l = &line[i];  // `__data` -> 8-bit pointer
-    if (l->arrives)
+    __data Subway *const l           = &line[i];  // `__data` -> 8-bit pointer
+    __data const uint8_t currentStop = l->stops[l->at];
+
+    l->running = !l->running;
+
+    // 1. Arrived -> Running (running == true)
+    //    - Set current stop to `color`
+    //    - Move to next stop
+    // 2. Running -> Arrived (running == false)
+    //    - Set current stop to `arrivalColor`
+    if (l->running)
     {
-        setColor(l->stops[l->at], l->colors[SUBWAY_COLOR]);
+        const uint8_t length = l->length;
+
+        setColor(currentStop, l->color);
         if (forward)
         {
-            if (++l->at >= l->length)
+            if (++l->at >= length)
             {
                 l->at = 0;
             }
@@ -94,69 +115,64 @@ void runSubway(uint8_t i, __bit forward)
         {
             if (l->at == 0)
             {
-                l->at = l->length;
+                l->at = length;
             }
             l->at--;
         }
-        setColor(l->stops[l->at], l->colors[SUBWAY_ARRIVAL_COLOR]);
-
-        ledChanged = 1;
     }
     else
     {
+        setColor(currentStop, l->arrivalColor);
+
         if (i == 0)
         {
-            setColor(gates[l->at & 0x01], RED);
-            setColor(gates[(l->at + 1) & 0x01], GREEN);
-            ledChanged = 1;
+            uint8_t at = l->at;
+            setColor(gates[at++ & 0x01], RED);
+            setColor(gates[at & 0x01], GREEN);
         }
     }
 
-    l->arrives = !l->arrives;
+    ledChanged = 1;
 
     idleTimer = 0;  // Reset idleTimer
 }
 
 void blinkLights()
 {
+    static uint8_t blinkTimer;
+
     for (uint8_t i = 0; i < 2; i++)
     {
         __data Subway *l = &line[i];  // `__data` -> 8-bit pointer
-        if (!l->arrives)              // Blinks
+        if (l->running)               // Blinks
         {
-            l->blinkTimer++;
-            if (l->blinkTimer == 10)  // Turn on
+            if (blinkTimer == 0)  // Turn on
             {
-                setColor(l->stops[l->at], l->colors[SUBWAY_ARRIVAL_COLOR]);
+                setColor(l->stops[l->at], l->arrivalColor);
                 ledChanged = 1;
             }
-            else if (l->blinkTimer == 20)  // Turn off
+            else if (blinkTimer == 10)  // Turn off
             {
-                l->blinkTimer = 0;
                 setColor(l->stops[l->at], BLACK);
                 ledChanged = 1;
             }
         }
-        else  // Reset from blinking
-        {
-            if (l->blinkTimer < 10)
-            {
-                l->blinkTimer = 10;
-                setColor(l->stops[l->at], l->colors[SUBWAY_ARRIVAL_COLOR]);
-                ledChanged = 1;
-            }
-        }
+    }
+
+    if (++blinkTimer >= 20)
+    {
+        blinkTimer = 0;
     }
 }
 
 void processEvents()
 {
     // All key pins are pulled up.
-    static uint8_t previousP1 = 0xFF;
+    static uint8_t lastP1 = 0xFF;
 
     if (KEY_RELEASED(KEY_A_PIN))
     {
-        LATCH = 1;  // Power off
+        KILL = 1;  // Power off
     }
     if (KEY_RELEASED(KEY_B_PIN))
     {
@@ -176,7 +192,7 @@ void processEvents()
     }
 
     // Debounce
-    previousP1 = P1;
+    lastP1 = P1;
     mDelaymS(DEBOUNCE_INTERVAL);
 }
 
@@ -186,10 +202,9 @@ void init()
     mDelaymS(5);
 
     // Configure P3.0 and P3.4 as GPIO output
-    P3_MOD_OC &= ~((1 << LATCH_PIN) & (1 << LED_PIN));
-    P3_DIR_PU |= (1 << LATCH_PIN) | (1 << LED_PIN);
-    LATCH = 0;  // Latch power on
-    LED   = 0;
+    P3_MOD_OC &= ~((1 << KILL_PIN) & (1 << LED_PIN));
+    P3_DIR_PU |= (1 << KILL_PIN) | (1 << LED_PIN);
+    KILL = 0;  // Power on
 
     // Configure P1.1, P1.4, P1.5, P1.6, and P1.7 as input.
     P1_MOD_OC &= ~((1 << KEY_A_PIN) & (1 << KEY_B_PIN) & (1 << KEY_C_PIN) & (1 << KEY_D_PIN) & (1 << KEY_E_PIN));
@@ -208,7 +223,7 @@ void main()
         // 6,000 loops is around 60 seconds.
         if (idleTimer++ >= 6000)
         {
-            LATCH = 1;  // Power off
+            KILL = 1;  // Power off
         }
 
         processEvents();
