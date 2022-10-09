a8949b456706a8f32e637dd71ebc31a11045c870
Mingjie Li <limingjie@outlook.com>
2022-10-09 23:50:30 +0800

    Get more accurate time by timer0 and use it everywhere

--- main.c
+++ main.c
@@ -16,29 +16,40 @@
 // All key pins are pulled up.
 // - A key pressed if input bit changes from 1 to 0.
 // - A key released if input bit changes from 0 to 1.
-#define KEY_PRESSED(KEY)  ((lastP1 & (1 << (KEY))) && !(P1 & (1 << (KEY))))
-#define KEY_RELEASED(KEY) (!(lastP1 & (1 << (KEY))) && (P1 & (1 << (KEY))))
+#define KEY_PRESSED(KEY)  ((lastKeyState & (1 << (KEY))) && !(P1 & (1 << (KEY))))
+#define KEY_RELEASED(KEY) (!(lastKeyState & (1 << (KEY))) && (P1 & (1 << (KEY))))
 #define DEBOUNCE_INTERVAL 8  // 8 ms
 
 // Output Pins
-#define LED_PIN  0  // P3.0 - WS2812 LED Strip
-#define KILL_PIN 3  // P3.3 - Power control
+#define LED_PIN      0  // P3.0 - WS2812 LED Strip
+#define SHUTDOWN_PIN 3  // P3.3 - Power control
 
 // Define bits
-SBIT(LED, 0xB0, LED_PIN);    // P3.0
-SBIT(KILL, 0xB0, KILL_PIN);  // P3.3
+SBIT(LED, 0xB0, LED_PIN);            // P3.0
+SBIT(SHUTDOWN, 0xB0, SHUTDOWN_PIN);  // P3.3
+
+// All the time variables are in milliseconds
+__data uint32_t systemTime     = 0;
+__data uint32_t lastActionTime = 0;
+// __data uint32_t previousTime   = 0;
+
+// Menu
+// 0 - Subway
+// 1 - Blink LEDs
+__data uint8_t menuIndex = 0;
 
 // WS2812
 #define LED_COUNT  27
 #define BRIGHTNESS 5
 __xdata uint8_t      ledData[LED_COUNT * 3];  // In external data memory
-__bit                ledChanged = 0;
-__data const uint8_t RED[3]     = {BRIGHTNESS, 0, 0};
-__data const uint8_t GREEN[3]   = {0, BRIGHTNESS, 0};
-__data const uint8_t BLUE[3]    = {0, 0, BRIGHTNESS};
-__data const uint8_t YELLOW[3]  = {BRIGHTNESS, BRIGHTNESS, 0};
-__data const uint8_t PURPLE[3]  = {BRIGHTNESS, 0, BRIGHTNESS};
-__data const uint8_t BLACK[3]   = {0, 0, 0};
+__bit                ledDataChanged = 0;
+__data const uint8_t RED[3]         = {BRIGHTNESS, 0, 0};
+__data const uint8_t GREEN[3]       = {0, BRIGHTNESS, 0};
+__data const uint8_t BLUE[3]        = {0, 0, BRIGHTNESS};
+__data const uint8_t YELLOW[3]      = {BRIGHTNESS, BRIGHTNESS, 0};
+__data const uint8_t PURPLE[3]      = {BRIGHTNESS, 0, BRIGHTNESS};
+__data const uint8_t BLACK[3]       = {0, 0, 0};
+__data const uint8_t BRIGHT[3]      = {255, 255, 255};
 
 typedef struct
 {
@@ -78,19 +89,16 @@ __data Subway line[2] = {
     },
 };
 
-__data const uint8_t gates[2] = {25, 26};
+__data const uint8_t subwayGates[2] = {25, 26};
 
-uint16_t idleTimer = 0;
-
-__xdata const uint8_t THE_STAR[] = {
+__xdata const uint8_t theStarSong[] = {
     14,                                               // length
     C4, 2, C4, 2, G4, 2, G4, 2, A4, 2, A4, 2, G4, 4,  // 1 1 | 5 5 | 6 6 | 5 -
     F4, 2, F4, 2, E4, 2, E4, 2, D4, 2, D4, 2, C4, 4   // 4 4 | 3 3 | 2 2 | 1 -
 };
-
-__xdata const uint8_t STARTUP_SOUND[]  = {5, E5, 2, B4, 2, A4, 3, E5, 2, B4, 4};
-__xdata const uint8_t SHUTDOWN_SOUND[] = {4, A5, 1, E5, 1, A4, 1, B4, 2};
-__xdata const uint8_t WARNING_SOUND[]  = {3, C4, 2, C4, 2, C4, 2};
+__xdata const uint8_t startupSound[]  = {5, E5, 2, B4, 2, A4, 3, E5, 2, B4, 4};
+__xdata const uint8_t shutdownSound[] = {4, A5, 1, E5, 1, A4, 1, B4, 2};
+__xdata const uint8_t warningSound[]  = {3, C4, 2, C4, 2, C4, 2};
 
 void setColor(uint8_t index, __data const uint8_t *color)  // `__data` -> 8-bit pointer
 {
@@ -107,7 +115,7 @@ void initSubway()
         setColor(i, YELLOW);
     }
 
-    ledChanged = 1;
+    ledDataChanged = 1;
 }
 
 void runSubway(uint8_t i, __bit forward)
@@ -150,79 +158,117 @@ void runSubway(uint8_t i, __bit forward)
         if (i == 0)
         {
             uint8_t at = l->at;
-            setColor(gates[at & 0x01], GREEN);
-            setColor(gates[++at & 0x01], RED);
+            setColor(subwayGates[at & 0x01], YELLOW);
+            setColor(subwayGates[++at & 0x01], BLUE);
         }
     }
 
-    ledChanged = 1;
-
-    idleTimer = 0;  // Reset idleTimer
+    ledDataChanged = 1;
+    lastActionTime = systemTime;
 }
 
-void blinkLights()
+void blinkSubwayLights()
 {
-    static uint8_t blinkTimer;
+    __xdata static uint32_t lastBlinkTime = 0;
+    static __bit            on            = 0;
 
-    for (uint8_t i = 0; i < 2; ++i)
+    if (systemTime - lastBlinkTime >= (on ? 700 : 300))
     {
-        __data Subway *l = &line[i];  // `__data` -> 8-bit pointer
-        if (l->running)               // Blinks
+        for (uint8_t i = 0; i < 2; ++i)
         {
-            if (blinkTimer == 0)  // Turn on
+            __data Subway *l = &line[i];  // `__data` -> 8-bit pointer
+            if (l->running)
             {
-                setColor(l->stops[l->at], l->arrivalColor);
-                ledChanged = 1;
-            }
-            else if (blinkTimer == 10)  // Turn off
-            {
-                setColor(l->stops[l->at], BLACK);
-                ledChanged = 1;
+                setColor(l->stops[l->at], on ? BLACK : l->arrivalColor);
             }
         }
-    }
 
-    if (++blinkTimer >= 20)
-    {
-        blinkTimer = 0;
+        on             = !on;
+        ledDataChanged = 1;
+        lastBlinkTime  = systemTime;
     }
 }
 
 void shutdown()
 {
-    playBuzzer(SHUTDOWN_SOUND);
-    KILL = 1;
+    playBuzzer(shutdownSound);
+    SHUTDOWN = 1;
+}
+
+void timer0_interrupt(void) __interrupt(INT_NO_TMR0)
+{
+    // (0x10000 - 0xFC18) x (1 / (12000000 / 12))) = 0.001s
+    TH0 = 0xFC;
+    TL0 = 0x18;
+
+    systemTime++;
+}
+
+void blinkLights()
+{
+    __xdata static uint32_t lastBlinkTime = 0;
+    static uint8_t          blinkCounter  = 0;
+
+    // Blink every 500ms
+    if (systemTime - lastBlinkTime >= 500)
+    {
+        __bit on = blinkCounter++ & 0x01;
+        for (uint8_t i = 0; i < LED_COUNT; i++)
+        {
+            setColor(i, on ? YELLOW : BLACK);
+            on = !on;
+        }
+
+        ledDataChanged = 1;
+        lastBlinkTime  = systemTime;
+    }
 }
 
 void processEvents()
 {
-    // All key pins are pulled up.
-    static uint8_t lastP1 = 0xFF;
+    __xdata static uint32_t lastDebounceTime = 0;
+    static uint8_t          lastKeyState     = 0xFF;  // All key pins are pulled up.
 
-    if (KEY_RELEASED(KEY_A_PIN))
+    if (systemTime - lastDebounceTime >= DEBOUNCE_INTERVAL)
     {
-        runSubway(0, 0);
-    }
-    if (KEY_RELEASED(KEY_B_PIN))
-    {
-        runSubway(0, 1);
-    }
-    if (KEY_RELEASED(KEY_C_PIN))
-    {
-        runSubway(1, 0);
-    }
-    if (KEY_RELEASED(KEY_D_PIN))
-    {
-        runSubway(1, 1);
-    }
-    if (KEY_RELEASED(KEY_E_PIN))
-    {
-        playBuzzer(THE_STAR);
-    }
+        if (KEY_RELEASED(KEY_A_PIN))
+        {
+            if (menuIndex == 0)
+                runSubway(0, 0);
+        }
+        if (KEY_RELEASED(KEY_B_PIN))
+        {
+            if (menuIndex == 0)
+                runSubway(0, 1);
+        }
+        if (KEY_RELEASED(KEY_C_PIN))
+        {
+            if (menuIndex == 0)
+                runSubway(1, 0);
+        }
+        if (KEY_RELEASED(KEY_D_PIN))
+        {
+            if (menuIndex == 0)
+                runSubway(1, 1);
+        }
+        if (KEY_RELEASED(KEY_E_PIN))
+        {
+            if (++menuIndex == 2)
+            {
+                menuIndex = 0;
+            }
 
-    // Debounce
-    lastP1 = P1;
-    mDelaymS(DEBOUNCE_INTERVAL);
+            // Reset subway lights
+            if (menuIndex == 0)
+            {
+                initSubway();
+            }
+            // playBuzzer(theStarSong);
+        }
+
+        lastKeyState     = P1;
+        lastDebounceTime = systemTime;
+    }
 }
 
 void startup()
@@ -233,9 +279,9 @@ void startup()
     // Configure P3.0 and P3.3 in push-pull output mode.
     // - Pn_MOD_OC = 0 (Push-Pull)
     // - Pn_DIR_PU = 1 (Output)
-    P3_MOD_OC &= ~((1 << KILL_PIN) | (1 << LED_PIN));
-    P3_DIR_PU |= (1 << KILL_PIN) | (1 << LED_PIN);
-    KILL = 0;  // Power on
+    P3_MOD_OC &= ~((1 << SHUTDOWN_PIN) | (1 << LED_PIN));
+    P3_DIR_PU |= (1 << SHUTDOWN_PIN) | (1 << LED_PIN);
+    SHUTDOWN = 0;  // Power on
 
     // Configure P1.1, P1.4, P1.5, P1.6, and P1.7 in Quasi-Bidirectional mode, support input with internal pull-up.
     // - Pn_MOD_OC = 1 (Open-Drain)
@@ -256,18 +302,29 @@ void startup()
     ADC_CHAN1 = 1;
     P3_DIR_PU &= ~bAIN3;
 
+    // Enable interrupt globally
+    EA = 1;
+    // Enable timer0 interrupt
+    ET0 = 1;
+    // Set timer0 to Mode 1 - 16-bit counter
+    // - The reset value of TMOD = 0x00
+    // - Set bT0_M1 = 0 and bT0_M0 = 1
+    TMOD |= bT0_M0;
+    // Start timer 0
+    TR0 = 1;
+
     // Play startup sound
     initBuzzer();
-    playBuzzer(STARTUP_SOUND);
+    playBuzzer(startupSound);
 }
 
 void batteryCheck()
 {
-    static uint16_t batteryMonitorTimer = 0;
-    static uint8_t  batteryLowCount     = 0;
+    static uint16_t lastCheckTime   = 0;
+    static uint8_t  batteryLowCount = 0;
 
-    // Check battery voltage on the first second of every checking cycle.
-    if (batteryMonitorTimer == 100)
+    // Check battery voltage every 10 seconds
+    if (systemTime - lastCheckTime >= 10000)
     {
         ADC_START = 1;     // Start ADC sampling.
         while (ADC_START)  // Sampling completes when ADC_START becomes 0.
@@ -279,7 +336,7 @@ void batteryCheck()
         // Assume the voltage supply is 3.3V: 255 x 1.5V / 3.3V = 115.9
         if (ADC_DATA < 116)  // Low voltage detected
         {
-            playBuzzer(WARNING_SOUND);
+            playBuzzer(warningSound);
             if (++batteryLowCount >= 3)
             {
                 shutdown();
@@ -289,28 +346,20 @@ void batteryCheck()
         {
             batteryLowCount = 0;
         }
-    }
 
-    // Set checking cycle, reset timer every around 10 seconds
-    if (++batteryMonitorTimer == 1000)
-    {
-        batteryMonitorTimer = 0;
+        lastCheckTime = systemTime;
     }
 }
 
 void main()
 {
     startup();
-
     initSubway();
 
     while (1)
     {
         // Power off after idle for 5 minutes.
-        // Each loop takes around 10 milliseconds, 6,000 loops is around 60 seconds.
-        // Use 8-bit instructions to save a few bytes for the binary.
-        // 6,000 x 5 >> 8 = 117 and 117 << 8 = 29,952, close enough.
-        if ((uint8_t)((++idleTimer) >> 8) >= 117)
+        if (systemTime - lastActionTime >= 300000)
         {
             shutdown();
         }
@@ -319,12 +368,23 @@ void main()
 
         processEvents();
 
-        blinkLights();
-
-        if (ledChanged)
+        switch (menuIndex)
         {
+            case 0:
+                blinkSubwayLights();
+                break;
+            case 1:
+                blinkLights();
+                break;
+        }
+
+        if (ledDataChanged)
+        {
+            EA = 0;  // Disable interrupt globally, to avoid interrupting WS2812 data transmission.
             bigBangWS2812(LED_COUNT, ledData);
-            ledChanged = 0;
+            EA = 1;  // Re-enable interrupt globally
+
+            ledDataChanged = 0;
         }
     }
 }
