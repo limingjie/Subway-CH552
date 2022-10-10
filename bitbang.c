ad13b9ff973fc2a58273687fc9b36276199f489c
Mingjie Li <limingjie@outlook.com>
2022-10-10 13:27:09 +0800

    Convert assembly to strings to avoid editor format the code.

--- bitbang.c
+++ bitbang.c
@@ -20,92 +20,87 @@ void bigBangWS2812(uint8_t ledCount, __xdata uint8_t* ledData)
 
     //  TODO: What context needs to be pushed/popped?
 
-    __asm
+    __asm__(
+        "mov r2, dpl             ; Load the LED count into r2                                \n"
 
-    mov r2, dpl             ; Load the LED count into r2
+        "mov dpl, _bigBangWS2812_PARM_2  ; Load the LED data start address into DPTR         \n"
+        "mov dph, (_bigBangWS2812_PARM_2 + 1)                                                \n"
 
-    mov dpl, _bigBangWS2812_PARM_2  ; Load the LED data start address into DPTR
-    mov dph, (_bigBangWS2812_PARM_2 + 1)
+        "00001$:                 ; byte loop                                                 \n"
 
-    00001$:                 ; byte loop
+        // Red byte
+        "    movx a,@dptr        ; Load the current LED data value into the accumulator (1)  \n"
+        "    inc dptr            ; and advance the counter for the next LED data value (1)   \n"
 
-// Red byte
-        movx a,@dptr        ; Load the current LED data value into the accumulator (1)
-        inc dptr            ; and advance the counter for the next LED data value (1)
+        "    mov r3, #8          ; Set up the bit loop (2)                                   \n"
+        "00002$:                 ; red bit loop                                              \n"
+        "    setb _LED           ; Begin bit cycle- set bit high (2)                         \n"
 
-        mov r3, #8          ; Set up the bit loop (2)
-    00002$:                 ; red bit loop
-        setb _LED           ; Begin bit cycle- set bit high (2)
+        "    nop                 ; Tune this count by hand, want ~.4uS (1*2)                 \n"
+        "    nop                                                                             \n"
 
-        nop                 ; Tune this count by hand, want ~.4uS (1*2)
-        nop
+        "    rlc A               ; Shift the LED data value left to get the high bit (1)     \n"
+        "    mov _LED, C         ; Set the output bit high if the current bit is high, (2)   \n"
+        "                        ; otherwise set it low                                      \n"
 
-        rlc A               ; Shift the LED data value left to get the high bit (1)
-        mov _LED, C         ; Set the output bit high if the current bit is high, (2)
-                            ; otherwise set it low
+        "    nop                 ; Tune this count by hand, want ~.4uS (1*2)                 \n"
+        "    nop                                                                             \n"
 
-        nop                 ; Tune this count by hand, want ~.4uS (1*2)
-        nop
+        "    clr _LED            ; final part of bit cycle, set bit low (2)                  \n"
 
-        clr _LED            ; final part of bit cycle, set bit low (2)
+        // "    nop                 ; Tune this count by hand, want ~.45uS                      \n"
+        // "    nop                                                                             \n"
 
-//        nop                 ; Tune this count by hand, want ~.45uS
-//        nop
+        "    djnz r3, 00002$     ; If there are more bits in this byte (2, ?)                \n"
 
-        djnz r3, 00002$     ; If there are more bits in this byte (2, ?)
+        // Green byte
+        "    movx a,@dptr        ; Load the current LED data value into the accumulator (1)  \n"
+        "    inc dptr            ; and advance the counter for the next LED data value (1)   \n"
 
-// Green byte
-        movx a,@dptr        ; Load the current LED data value into the accumulator (1)
-        inc dptr            ; and advance the counter for the next LED data value (1)
+        "    mov r3, #8          ; Set up the bit loop (2)                                   \n"
+        "00003$:                 ; red bit loop                                              \n"
+        "    setb _LED           ; Begin bit cycle- set bit high (2)                         \n"
 
-        mov r3, #8          ; Set up the bit loop (2)
-    00003$:                 ; red bit loop
-        setb _LED           ; Begin bit cycle- set bit high (2)
+        "    nop                 ; Tune this count by hand, want ~.4uS (1*2)                 \n"
+        "    nop                                                                             \n"
 
-        nop                 ; Tune this count by hand, want ~.4uS (1*2)
-        nop
+        "    rlc A               ; Shift the LED data value left to get the high bit (1)     \n"
+        "    mov _LED, C         ; Set the output bit high if the current bit is high, (2)   \n"
+        "                        ; otherwise set it low                                      \n"
 
-        rlc A               ; Shift the LED data value left to get the high bit (1)
-        mov _LED, C         ; Set the output bit high if the current bit is high, (2)
-                            ; otherwise set it low
+        "    nop                 ; Tune this count by hand, want ~.4uS (1*2)                 \n"
+        "    nop                                                                             \n"
 
-        nop                 ; Tune this count by hand, want ~.4uS (1*2)
-        nop
+        "    clr _LED            ; final part of bit cycle, set bit low (2)                  \n"
 
-        clr _LED            ; final part of bit cycle, set bit low (2)
+        // "    nop                 ; Tune this count by hand, want ~.45uS                      \n"
+        // "    nop                                                                             \n"
 
-//        nop                 ; Tune this count by hand, want ~.45uS
-//        nop
+        "    djnz r3, 00003$     ; If there are more bits in this byte (2, ?)                \n"
 
-        djnz r3, 00003$     ; If there are more bits in this byte (2, ?)
+        // Blue byte
+        "    movx a,@dptr        ; Load the current LED data value into the accumulator (1)  \n"
+        "    inc dptr            ; and advance the counter for the next LED data value (1)   \n"
 
-// Blue byte
-        movx a,@dptr        ; Load the current LED data value into the accumulator (1)
-        inc dptr            ; and advance the counter for the next LED data value (1)
+        "    mov r3, #8          ; Set up the bit loop (2)                                   \n"
+        "00004$:                 ; red bit loop                                              \n"
+        "    setb _LED           ; Begin bit cycle- set bit high (2)                         \n"
 
-        mov r3, #8          ; Set up the bit loop (2)
-    00004$:                 ; red bit loop
-        setb _LED           ; Begin bit cycle- set bit high (2)
+        "    nop                 ; Tune this count by hand, want ~.4uS (1*2)                 \n"
+        "    nop                                                                             \n"
 
-        nop                 ; Tune this count by hand, want ~.4uS (1*2)
-        nop
+        "    rlc A               ; Shift the LED data value left to get the high bit (1)     \n"
+        "    mov _LED, C         ; Set the output bit high if the current bit is high, (2)   \n"
+        "                        ; otherwise set it low                                      \n"
 
-        rlc A               ; Shift the LED data value left to get the high bit (1)
-        mov _LED, C         ; Set the output bit high if the current bit is high, (2)
-                            ; otherwise set it low
+        "    nop                 ; Tune this count by hand, want ~.4uS (1*2)                 \n"
+        "    nop                                                                             \n"
 
-        nop                 ; Tune this count by hand, want ~.4uS (1*2)
-        nop
+        "    clr _LED            ; final part of bit cycle, set bit low (2)                  \n"
 
-        clr _LED            ; final part of bit cycle, set bit low (2)
+        // "    nop                 ; Tune this count by hand, want ~.45uS                      \n"
+        // "    nop                                                                             \n"
+        "    djnz r3, 00004$     ; If there are more bits in this byte (2, ?)                \n"
 
-//        nop                 ; Tune this count by hand, want ~.45uS
-//        nop
-
-        djnz r3, 00004$     ; If there are more bits in this byte (2, ?)
-
-
-        djnz r2, 00001$     ; If there are more LEDs (2, ?)
-
-    __endasm;
+        "    djnz r2, 00001$     ; If there are more LEDs (2, ?)                             \n");
 }
